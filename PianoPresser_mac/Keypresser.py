d777d9f1ca5b9e3c5fa4805028fa2d4dc145c947
modpro361 <modpro361@gmail.com>
2026-07-22 14:12:51 +1200

    added midi to jianpu conversion and added charactor map on mac autoclicker

--- PianoPresser_mac/Keypresser.py
+++ PianoPresser_mac/Keypresser.py
@@ -31,13 +31,16 @@ def bring_nte_to_front(app_name):
             
     return f"Application '{app_name}' is not currently running."
 
-
-print(bring_nte_to_front("Calculator"))
+# Character map
+char_map = {
+    'q': 0x10, 'w': 0x11, 'e': 0x12, 'r': 0x13, 't': 0x14, 'z': 0x2C, 'u': 0x16, 'i': 0x17, 'o': 0x18, 'p': 0x19,
+    'a': 0x1E, 's': 0x1F, 'd': 0x20, 'f': 0x21, 'g': 0x22, 'h': 0x23, 'j': 0x24, 'k': 0x25, 'l': 0x26,
+    'y': 0x15, 'x': 0x2D, 'c': 0x2E, 'v': 0x2F, 'b': 0x30, 'n': 0x31, 'm': 0x32, 'shift': 0x2A, 'ctrl': 0x1D }
 
 
 # Example Usage: Simulates pressing the letter 'A' (Keycode 0) after a short safety delay
 if __name__ == "__main__":
-    print("Starting in 3 seconds... Click inside your target text box or application.")
+    print(bring_nte_to_front("Calculator"))
     time.sleep(3)
     
     print("Injecting keypress...")
